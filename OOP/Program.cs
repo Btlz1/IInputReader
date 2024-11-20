956dcd384b0939949010d1cd09c097f81a4b6e0e
Btlz1 <bealtes0@gmail.com>
2024-11-20 21:14:56 +0500

    Добавьте файлы проекта.

---
+++ OOP/Program.cs
@@ -0,0 +1,53 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Используем ConstantReader
+        IInputReader constantReader = new ConstantReader();
+        IntegerParser constantParser = new IntegerParser(constantReader);
+        Console.WriteLine($"Parsed value from constant: {constantParser.Parse()}");
+
+        // Используем ConsoleReader
+        IInputReader consoleReader = new ConsoleReader();
+        IntegerParser consoleParser = new IntegerParser(consoleReader);
+        Console.Write("Please enter a number:");
+        Console.WriteLine($"Parsed value from console: {consoleParser.Parse()}");
+    }
+}
+interface IInputReader
+{
+    string ReadFromSource();
+}
+internal class ConstantReader : IInputReader
+{
+    public string ReadFromSource()
+    {
+        return "5";
+    } 
+}
+internal class ConsoleReader : IInputReader
+{
+    public string ReadFromSource()
+    {
+        string userInput;
+        userInput = Console.ReadLine();
+        return userInput;
+
+    }
+}
+class IntegerParser
+{
+    private readonly IInputReader _inputReader;
+
+    // Конструктор принимает IInputReader
+    public IntegerParser(IInputReader inputReader)
+    {
+        _inputReader = inputReader;
+    }
+
+    public int Parse()
+    {
+        var inputedValue = _inputReader.ReadFromSource(); // Используем переданный IInputReader
+        return int.Parse(inputedValue);
+    }
+}
